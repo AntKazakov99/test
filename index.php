ee010ce2321d88af29bc062f7c85ad3f346177f4
Anton Kazakov <antkazakov@team.amocrm.com>
2024-12-26 15:35:48 +0300

    Script for transferring data from mail to CRM

--- index.php
+++ index.php
@@ -1,79 +1,65 @@
 <?php
 
-use Symfony\Component\Dotenv\Dotenv;
+use AmoCRM\Collections\Leads\LeadsCollection;
+use Ant\Application;
 
 include_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__ . '/.env');
+$maxExecTime = ini_get('max_execution_time');
 
-$mailbox = $_ENV['MAILBOX'];
-$user = $_ENV['USER'];
-$password = $_ENV['PASSWORD'];
+$app = Application::getInstance();
 
-$inbox = imap_open($mailbox, $user, $password);
+$cacheFilePath = __DIR__ . '/tmp/cache.json';
 
-$emails = imap_search($inbox, 'ALL');
+if (file_exists($cacheFilePath)) {
+    $data = json_decode(file_get_contents($cacheFilePath), true);
+}
 
-if ($emails) {
-    // Sort emails by newest first
-    rsort($emails);
+$emailIds = isset($data)  ? $data['ids'] : $app->requestEmailIds();
 
-    echo '<pre>';
-    // Loop through each email
-    foreach ($emails as $email_number) {
-        $searchPatterns = [
-            [
-                'header' => 'Автор запроса',
-                'code' => 'author'
-            ],
-            [
-                'header' => 'Кому выдать доступ',
-                'code' => 'receiver'
-            ],
-            [
-                'header' => 'Общие доступы',
-                'code' => 'category'
-            ],
-            [
-                'header' => 'Комментарий',
-                'code' => 'commentary'
-            ]
-        ];
+$startTime = microtime(true);
 
-        $overview = imap_fetch_overview($inbox, $email_number);
-
-        $message = imap_fetchbody($inbox, $email_number, 1);
-
-        $dom = new DOMDocument();
-        @$dom->loadHTML($message);
-
-        $rows = $dom->getElementsByTagName('tr');
-
-        $messageData = [];
-        foreach ($rows as $row) {
-            $cols = $row->getElementsByTagName('td');
-            if (count($cols) !== 2) {
-                continue;
-            }
-
-            foreach ($searchPatterns as $pattern) {
-                if (str_contains($cols[0]->nodeValue, $pattern['header'])) {
-                    $messageData[$pattern['code']] = $cols[1]->nodeValue;
-                }
-            }
-        }
-
-        foreach ($searchPatterns as $pattern) {
-            echo $pattern['header'] . ': ' . (array_key_exists($pattern['code'], $messageData) ? $messageData[$pattern['code']] : '-') . PHP_EOL;
-        }
-
-        $messageData['requestDate'] = $overview[0]->udate;
-
-        echo 'Дата запроса: ' . date('d.m.Y H:i:s', $messageData['requestDate']) . PHP_EOL;
-        echo '----' . PHP_EOL;
-        echo PHP_EOL;
+$emails = isset($data) ? $data['emails'] : [];
+foreach ($emailIds as $key => $emailId) {
+    if (microtime(true) - $startTime >= $maxExecTime - 10) {
+        file_put_contents($cacheFilePath, json_encode(['ids' => $emailIds, 'current' => $key, 'emails' => $emails]));
+        header('Location: ' . $_SERVER['REQUEST_URI']);
+        die();
     }
-} else {
-    echo "No emails found.\n";
+
+    if (isset($data) && $key < $data['current']) {
+        continue;
+    }
+
+    $parsedEmail = $app->parseEmail($emailId);
+
+    if (!is_null($parsedEmail)) {
+        $emails[] = $parsedEmail;
+    }
+}
+
+file_put_contents($cacheFilePath, json_encode(['ids' => $emailIds, 'current' => count($emails), 'emails' => $emails]));
+
+$leads = new LeadsCollection();
+foreach ($emails as $email) {
+    try {
+        $leads->add(
+            $app->createLeadFromData([
+                'author' => $email['author'],
+                'receiver' => $email['receiver'],
+                'date' => $email['requestDate'],
+                'category' => $email['category'] ?: $email['commentary']
+            ])
+            ->setName('Application for access')
+        );
+    } catch (\AmoCRM\Exceptions\InvalidArgumentException $e) {
+        var_dump($e->getMessage());
+    }
+    break;
+}
+
+var_dump($leads);
+
+foreach ($leads->chunk(50) as $chunk) {
+    $app->getAmoCrmApiClient()->leads()->add($chunk);
 }
---
+++ lib/Application.php
@@ -0,0 +1,312 @@
+<?php
+
+namespace Ant;
+
+use AmoCRM\Client\AmoCRMApiClient;
+use AmoCRM\Collections\CustomFieldsValuesCollection;
+use AmoCRM\Exceptions\AmoCRMoAuthApiException;
+use AmoCRM\Exceptions\InvalidArgumentException;
+use AmoCRM\Models\CustomFieldsValues\DateCustomFieldValuesModel;
+use AmoCRM\Models\CustomFieldsValues\TextareaCustomFieldValuesModel;
+use AmoCRM\Models\CustomFieldsValues\TextCustomFieldValuesModel;
+use AmoCRM\Models\CustomFieldsValues\ValueCollections\DateCustomFieldValueCollection;
+use AmoCRM\Models\CustomFieldsValues\ValueCollections\TextareaCustomFieldValueCollection;
+use AmoCRM\Models\CustomFieldsValues\ValueCollections\TextCustomFieldValueCollection;
+use AmoCRM\Models\CustomFieldsValues\ValueModels\DateCustomFieldValueModel;
+use AmoCRM\Models\CustomFieldsValues\ValueModels\TextareaCustomFieldValueModel;
+use AmoCRM\Models\CustomFieldsValues\ValueModels\TextCustomFieldValueModel;
+use AmoCRM\Models\LeadModel;
+use DateTime;
+use DOMDocument;
+use Exception;
+use IMAP\Connection;
+use League\OAuth2\Client\Token\AccessToken;
+use Random\RandomException;
+use Symfony\Component\Dotenv\Dotenv;
+
+class Application
+{
+    /**
+     * todo change to actual file path
+     */
+    private const ACCESS_TOKEN_FILE_PATH = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR . 'access_token.json';
+    private static ?self $instance = null;
+    private ?array $config = null;
+    private ?AmoCRMApiClient $amoCrmApiClient = null;
+    private ?AccessToken $accessToken = null;
+    private ?Connection $imapConnection = null;
+    private const CUSTOM_FIELDS_IDS = [
+        'author' => 682713,
+        'receiver' => 682717,
+        'date' => 682705,
+        'category' => 769127
+    ];
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConfig(): array
+    {
+        if ($this->config === null) {
+            $dotenv = new Dotenv();
+            $dotenv->load(__DIR__ . '/../.env');
+            $this->config = $_ENV;
+        }
+        return $this->config;
+    }
+
+    /**
+     * @throws AmoCRMoAuthApiException
+     * @throws RandomException
+     */
+    public function getAmoCrmApiClient(): AmoCRMApiClient
+    {
+        if ($this->amoCrmApiClient === null) {
+            $config = $this->getConfig();
+            $this->amoCrmApiClient = new AmoCRMApiClient(
+                $config['CLIENT_ID'],
+                $config['CLIENT_SECRET'],
+                $config['CLIENT_REDIRECT_URI']
+            );
+            $this->amoCrmApiClient->setAccessToken($this->getAccessToken());
+            $this->amoCrmApiClient->setAccountBaseDomain($this->getBaseDomain());
+        }
+        return $this->amoCrmApiClient;
+    }
+
+    /**
+     * @throws AmoCRMoAuthApiException
+     * @throws RandomException
+     */
+    public function getAccessToken(): AccessToken
+    {
+        if ($this->accessToken === null) {
+            if (!file_exists($this::ACCESS_TOKEN_FILE_PATH)) {
+                return $this->generateAccessToken();
+            }
+            $accessTokenData = json_decode(file_get_contents($this::ACCESS_TOKEN_FILE_PATH), true);
+
+            if ($accessTokenData['expires'] < time()) {
+                return $this->generateAccessToken();
+            }
+
+            $this->accessToken = new AccessToken($accessTokenData);
+        }
+        return $this->accessToken;
+    }
+
+    /**
+     * @throws AmoCRMoAuthApiException
+     * @throws RandomException
+     * @throws Exception
+     */
+    public function generateAccessToken(): AccessToken
+    {
+        session_start();
+        if (!isset($_GET['code'])) {
+            // Redirect
+            $state = bin2hex(random_bytes(16));
+            $_SESSION['oauth2state'] = $state;
+            $authorizationUrl = $this->getAmoCrmApiClient()->getOAuthClient()->getAuthorizeUrl([
+                'state' => $state,
+                'mode' => 'post_message',
+            ]);
+            header('Location: ' . $authorizationUrl);
+            die;
+        } elseif (!isset($_GET['state']) || !isset($_SESSION['oauth2state']) || $_SESSION['oauth2state'] !== $_GET['state']) {
+            throw new Exception('Invalid state');
+        }
+
+        return $this->requestAccessToken($_GET['code'], $_GET['referer']);
+    }
+
+    /**
+     * @throws AmoCRMoAuthApiException
+     * @throws Exception
+     */
+    public function requestAccessToken(string $authCode, string $domain): AccessToken
+    {
+        $this->getAmoCrmApiClient()->setAccountBaseDomain($domain);
+        $accessToken = $this->getAmoCrmApiClient()->getOAuthClient()->getAccessTokenByCode($authCode);
+
+        if ($accessToken->hasExpired()) {
+            throw new Exception('Expired access token');
+        }
+
+        $tokenData = [
+            'access_token' => $accessToken->getToken(),
+            'expires' => $accessToken->getExpires(),
+            'refresh_token' => $accessToken->getRefreshToken(),
+            'base_domain' => $this->getAmoCrmApiClient()->getAccountBaseDomain()
+        ];
+
+        $dir = dirname($this::ACCESS_TOKEN_FILE_PATH);
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
+
+        file_put_contents($this::ACCESS_TOKEN_FILE_PATH, json_encode($tokenData));
+
+        return new AccessToken($tokenData);
+    }
+
+    /**
+     * @throws AmoCRMoAuthApiException
+     * @throws RandomException
+     */
+    public function getBaseDomain(): string
+    {
+        if (!file_exists($this::ACCESS_TOKEN_FILE_PATH)) {
+            return $this->getAmoCrmApiClient()->getAccountBaseDomain();
+        }
+
+        $accessTokenData = json_decode(file_get_contents($this::ACCESS_TOKEN_FILE_PATH), true);
+
+        return $accessTokenData['base_domain'];
+    }
+
+    // Работа с почтой
+
+    private const SEARCH_PATTERNS = [
+        [
+            'parsed' => true,
+            'header' => 'Автор запроса',
+            'code' => 'author'
+        ],
+        [
+            'parsed' => true,
+            'header' => 'Кому выдать доступ',
+            'code' => 'receiver'
+        ],
+        [
+            'parsed' => true,
+            'header' => 'Общие доступы',
+            'code' => 'category'
+        ],
+        [
+            'parsed' => true,
+            'header' => 'Комментарий',
+            'code' => 'commentary'
+        ]
+    ];
+
+    public function getImapConnection(): Connection
+    {
+        if (is_null($this->imapConnection)) {
+            $config = $this->getConfig();
+            $this->imapConnection = imap_open($config['MAILBOX'], $config['USER'], $config['PASSWORD']);
+        }
+        return $this->imapConnection;
+    }
+
+    public function requestEmailIds(string $criteria = 'ALL'): array
+    {
+        $emailIds = imap_search($this->getImapConnection(), $criteria) ?: [];
+        rsort($emailIds);
+        return $emailIds;
+    }
+
+    public function parseEmail(string $id): ?array
+    {
+        $data = [];
+
+        $overview = imap_fetch_overview($this->getImapConnection(), $id);
+        $message = imap_fetchbody($this->getImapConnection(), $id, 1);
+
+        $data['requestDate'] = $overview[0]->udate;
+
+        $dom = new DOMDocument();
+        @$dom->loadHTML($message);
+
+        $rows = $dom->getElementsByTagName('tr');
+
+        foreach ($rows as $row) {
+            $cols = $row->getElementsByTagName('td');
+            if (count($cols) !== 2) {
+                continue;
+            }
+
+            foreach (self::SEARCH_PATTERNS as $pattern) {
+                if (str_contains($cols[0]->nodeValue, $pattern['header'])) {
+                    $data[$pattern['code']] = $cols[1]->nodeValue;
+                }
+            }
+        }
+
+        if (!isset($data['author'])) {
+            // Второй парсер
+        }
+
+        if (!isset($data['author']) || !isset($data['receiver'])) {
+            return null;
+        }
+
+        return $data;
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function createLeadFromData(array $emailData): LeadModel
+    {
+        $leadFieldsValues = (new CustomFieldsValuesCollection())
+            ->add($this->createTextFieldValue(self::CUSTOM_FIELDS_IDS['author'], $emailData['author']))
+            ->add($this->createTextFieldValue(self::CUSTOM_FIELDS_IDS['receiver'], $emailData['receiver']))
+            ->add($this->createDateFieldValue(self::CUSTOM_FIELDS_IDS['date'], (int)$emailData['date']))
+            ->add($this->createTextareaFieldValue(self::CUSTOM_FIELDS_IDS['category'], $emailData['category']));
+
+        return (new LeadModel())
+            ->setCustomFieldsValues($leadFieldsValues);
+    }
+
+    public function createTextFieldValue(int $id, string $value): TextCustomFieldValuesModel
+    {
+        $fieldValue = (new TextCustomFieldValueModel())
+            ->setValue($value);
+
+        $valueCollection = (new TextCustomFieldValueCollection())
+            ->add($fieldValue);
+
+        return (new TextCustomFieldValuesModel())
+            ->setFieldId($id)
+            ->setValues($valueCollection);
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function createDateFieldValue(int $id, int $value): DateCustomFieldValuesModel
+    {
+        $fieldValue = (new DateCustomFieldValueModel())
+            ->setValue(DateTime::createFromFormat('U', $value));
+
+        $valueCollection = (new DateCustomFieldValueCollection())
+            ->add($fieldValue);
+
+        return (new DateCustomFieldValuesModel())
+            ->setFieldId($id)
+            ->setValues($valueCollection);
+    }
+
+    public function createTextareaFieldValue(int $id, string $value): TextareaCustomFieldValuesModel
+    {
+        $fieldValue = (new TextareaCustomFieldValueModel())
+            ->setValue($value);
+
+        $valueCollection = (new TextareaCustomFieldValueCollection())
+            ->add($fieldValue);
+
+        return (new TextareaCustomFieldValuesModel())
+            ->setFieldId($id)
+            ->setValues($valueCollection);
+    }
+}
